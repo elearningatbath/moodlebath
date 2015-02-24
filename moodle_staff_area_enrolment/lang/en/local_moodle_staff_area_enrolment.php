3f1b7e599bfa1955ce78df81f2d5e25fb41c4eb7
Hittesh Ahuja <h.ahuja@bath.ac.uk>
2015-02-24 23:21:54 +0000

    Removed them from the base folder

--- moodle_staff_area_enrolment/lang/en/local_moodle_staff_area_enrolment.php
+++
@@ -1,4 +0,0 @@
-<?php
-$string['pluginname'] = 'Moodle Staff Area Enrolment';
-$string['moodle_staff_area_label'] = 'Moodle Staff Area Course ID';
-$string['moodle_staff_area_desc'] = 'Please enter the Course ID number';

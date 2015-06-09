bfe5276a39557db2909e1bba81a242c3add11430
Hittesh Ahuja <h.ahuja@bath.ac.uk>
2015-06-09 11:56:30 +0100

    Added Bath alert container

--- theme/bath_bootstrap/layout/columns1.php
+++ theme/bath_bootstrap/layout/columns1.php
@@ -68,6 +68,12 @@ echo $OUTPUT->doctype() ?>
             <div class="breadcrumb-nav"><?php echo $OUTPUT->navbar(); ?></div>
             <nav class="breadcrumb-button"><?php echo $OUTPUT->page_heading_button(); ?></nav>
         </div>
+<!-- Bath Alert message when needed-->
+<?php 
+	if(!empty($CFG->alert)){
+?>
+	<div id="bath-alert"><span><?php echo $CFG->alert;?></span></div>
+<?php		} ?>
            <div id="page-content" class="row-fluid">
         <section id="region-main" class="span12">
             <?php
--- theme/bath_bootstrap/layout/columns3.php
+++ theme/bath_bootstrap/layout/columns3.php
@@ -67,6 +67,13 @@ echo $OUTPUT->doctype() ?>
             <div class="breadcrumb-nav"><?php echo $OUTPUT->navbar(); ?></div>
             <nav class="breadcrumb-button"><?php echo $OUTPUT->page_heading_button(); ?></nav>
         </div>
+<!-- Bath Alert message when needed-->
+<?php
+        if(!empty($CFG->alert)){
+?>
+        <div id="bath-alert"><span><?php echo $CFG->alert;?></span></div>
+<?php           } ?>
+
     <div id="page-content" class="row-fluid">
         <div id="<?php echo $regionbsid ?>" class="span9">
             <div class="row-fluid">

1e2538d0976a4d86468a849de85a92c3db52bf1f
Hittesh Ahuja <h.ahuja@bath.ac.uk>
2015-07-15 15:56:53 +0100

    Replaced  with

--- theme/bath_bootstrap/layout/columns1.php
+++ theme/bath_bootstrap/layout/columns1.php
@@ -70,9 +70,9 @@ echo $OUTPUT->doctype() ?>
         </div>
 <!-- Bath Alert message when needed-->
 <?php 
-	if(!empty($page->theme->settings->alertmessage)){
+	if(!empty($PAGE->theme->settings->alertmessage)){
 ?>
-	<div id="bath-alert"><span><?php echo $page->theme->settings->alertmessage; ?></span></div>
+	<div id="bath-alert"><span><?php echo $PAGE->theme->settings->alertmessage; ?></span></div>
 <?php		} ?>
            <div id="page-content" class="row-fluid">
         <section id="region-main" class="span12">

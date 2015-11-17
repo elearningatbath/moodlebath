aa47fdc56fff505004f62a4c85d28143abb965a8
sf427 <sf427@bath.ac.uk>
2015-11-17 17:06:59 +0000

    Fixes #65 overrides function can_hide_show_instance($instance) in the sits/lib.php and adds the file db/access.php

--- enrol/sits/db/access.php
+++ enrol/sits/db/access.php
@@ -1,4 +1,5 @@
 <?php
+
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -14,16 +15,18 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 /**
+ * Capabilities for sits access plugin.
  *
- * Fixes issue #65
- *
+ * @package    enrol_sits
+ * @copyright
+ * @license    University of Bath
  */
+
 defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
-    // Marks roles that have category role assignments synchronised to course enrolments
-    // overrides below system context are ignored (for performance reasons).
-    // By default his is not allowed in new installs, admins have to explicitly allow category enrolments.
+
+//fixes issue #65
 
     'enrol/sits:config' => array(
         'captype' => 'write',

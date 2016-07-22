c6dfd5f06ae65bc35e671dbe2e4f2dc3fe468f90
Hittesh Ahuja <ha386@bath.ac.uk>
2016-07-22 16:23:20 +0100

    Moved settings to config_plugins table

--- blocks/sits/settings.php
+++ blocks/sits/settings.php
@@ -11,22 +11,22 @@ $options = array(
     get_string('sits_enable_block_off','block_sits'),
     get_string('sits_enable_block_on','block_sits'),
 );
-$settings->add(new admin_setting_configselect('sits_gui_enabled', get_string('sits_gui_label', 'block_sits'),
+$settings->add(new admin_setting_configselect('sits/sits_gui_enabled', get_string('sits_gui_label', 'block_sits'),
 get_string('sits_gui_desc', 'block_sits'), 1, $options));
 
-$settings->add(new admin_setting_configtext('sits_disable_message', get_string('sits_disable_label', 'block_sits'),
+$settings->add(new admin_setting_configtext('sits/sits_disable_message', get_string('sits_disable_label', 'block_sits'),
 get_string('sits_disable_desc', 'block_sits'), get_string('sits_disable_text', 'block_sits'), $paramtype=PARAM_RAW, $size=70));
  
 $options = array(get_string('sits_enable_block_off','block_sits'), get_string('sits_option_daily','block_sits'), get_string('sits_option_continous','block_sits'));
 // Default whether user needs to mark a post as read
-$settings->add(new admin_setting_configselect('sits_cron_select', get_string('sits_cron_label', 'block_sits'),
+$settings->add(new admin_setting_configselect('sits/sits_cron_select', get_string('sits_cron_label', 'block_sits'),
 get_string('config_select_desc', 'block_sits'), 1, $options));
 
 $options = array(
     get_string('sits_enable_block_off','block_sits'),
     get_string('sits_enable_block_on','block_sits'),
 );
-$settings->add(new admin_setting_configselect('sits_remove_orphans', get_string('sits_orphan_label', 'block_sits'),
+$settings->add(new admin_setting_configselect('sits/sits_remove_orphans', get_string('sits_orphan_label', 'block_sits'),
 get_string('sits_orphan_desc', 'block_sits'), 1, $options));
 
 $options = array();
@@ -34,13 +34,13 @@ for ($i=0; $i<24; $i++) {
     $options[$i] = $i;
 }
 
-$settings->add(new admin_setting_configselect('sits_hour_of_sync', get_string('config_runtime_label', 'block_sits'),
+$settings->add(new admin_setting_configselect('sits/sits_hour_of_sync', get_string('config_runtime_label', 'block_sits'),
 get_string('config_runtime_desc', 'block_sits'), 1, $options));
 
 $days = array(1=>'Monday', 'Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday');
-$settings->add(new admin_setting_configmulticheckbox('sits_cron_days','Days to run cron on','Select the days you want the cron to run',array(),$days));
+$settings->add(new admin_setting_configmulticheckbox('sits/sits_cron_days','Days to run cron on','Select the days you want the cron to run',array(),$days));
 //Only add the settings if the local plugin exist
 if(file_exists($CFG->dirroot.'/local/moodle_staff_area_enrolment/lib.php')){
-	$settings->add(new admin_setting_configtext('sits_moodle_staff_area', get_string('sits_moodle_staff_area_label', 'block_sits'),
+	$settings->add(new admin_setting_configtext('sits/sits_moodle_staff_area', get_string('sits_moodle_staff_area_label', 'block_sits'),
 get_string('sits_moodle_staff_area_desc', 'block_sits'), NULL,PARAM_INT));
 }
